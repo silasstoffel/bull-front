ab75e1761dd016aaa7a70c00a72c02359cfa1b10
Silas Stoffel <silasstofel@gmail.com>
2023-03-11 11:06:10 -0300

    feat: adding skeleton app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
